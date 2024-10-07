fe1d3e4bf3886b51ebef2b5262f109be9460b09f
sarnali15 <191sarnali@gmail.com>
2024-10-07 23:53:45 +0600

    keyof constraint2

--- learn2/2.7-constraint-using-key-of.ts
+++ learn2/2.7-constraint-using-key-of.ts
@@ -10,6 +10,24 @@
 
     type Owner2 = keyof Vehicle
 
+    const getPropertyValue = <X, Y extends keyof X>
+        (obj: X, key: Y) => {
+        return obj[key];
+    }
+
+    const user = {
+        name: "Sarna",
+        age: 25,
+        address: 'ctg'
+    }
+
+    const car = {
+        model: "Toyota 100",
+        year: 200
+    }
+    const result1 = getPropertyValue(user, 'name')
+    const result2 = getPropertyValue(car, 'model')
+
     //const person1: Owner = "car"
 
     // const user ={
